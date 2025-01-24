58558ae2a92327b81aca88f81e0dd240a92da5cd
7beunseo <kimes0403@gmail.com>
2025-01-25 02:02:51 +0900

    💄 Design: 작품 리스트 페이지  반응형 구현

--- src/components/project/projectGroup/ProjectGroup.style.js
+++ src/components/project/projectGroup/ProjectGroup.style.js
@@ -1,7 +1,15 @@
 import styled from "styled-components";
 
 export const ProjectGroupContainer = styled.div`
-  margin: 0px 70px 20px 50px;
+  margin: 0px 0px 20px 50px;
+
+  @media (max-width: 768px) {
+    margin: 0px 0px 20px 35px;
+  }
+
+  @media (max-width: 480px) {
+    margin: 0px 0px 20px 30px;
+  }
 `;
 
 export const Title = styled.h3`
@@ -9,6 +17,18 @@ export const Title = styled.h3`
   margin-bottom: 20px;
   margin-left: 20px;
   weight: bold;
+
+  @media (max-width: 768px) {
+    margin-left: 24px;
+    font-size: 18px;
+    margin-bottom: 15px;
+  }
+
+  @media (max-width: 480px) {
+    margin-left: 4px;
+    font-size: 15px;
+    margin-bottom: 10px;
+  }
 `;
 
 export const ProjectListContainer = styled.div`
