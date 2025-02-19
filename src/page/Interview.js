d2941816982371060d4b8c8028b4fea2bb871ef0
eunkyoung <eunkyoung4160@gmail.com>
2025-02-20 00:31:21 +0900

    feat: 인터뷰 초안

--- src/page/Interview.js
+++ src/page/Interview.js
@@ -1,8 +1,9 @@
-export default function Interview(){
-    
-    return(
-        <>
-            <div>Interview</div>
-        </>
-    );
+import Title from "../components/title/title/Title";
+
+export default function Interview() {
+  return (
+    <>
+      <Title title="코너 인터뷰" subtitle="IT 소모임 Corner" color="blue" />
+    </>
+  );
 }
