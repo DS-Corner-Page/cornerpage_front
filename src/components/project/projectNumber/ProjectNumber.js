fba01c800e0373a24fffd81f1c9afa66b0098b23
mzxxzysy <jsyjj21@gmail.com>
2025-02-28 12:02:57 +0900

    Refactor : 은서랑 css 맞추기

--- src/components/project/projectNumber/ProjectNumber.js
+++ src/components/project/projectNumber/ProjectNumber.js
@@ -1,5 +1,6 @@
 import React from "react";
 import * as P from "./ProjectNumber.style";
+import * as A from "../../../styles/GalleryStyle";
 
 export default function ProjectNumber({ selectedProject, setSelectedProject }) {
   const handleClick = (number) => {
@@ -7,16 +8,22 @@ export default function ProjectNumber({ selectedProject, setSelectedProject }) {
   };
 
   return (
-    <P.ProjectNumberContainer>
+    <A.SideBar>
       {["4기", "3기", "2기", "1기"].map((item) => (
-        <P.ProjectNumberItem
-          key={item}
-          isSelected={selectedProject === item}
-          onClick={() => handleClick(item)}
-        >
-          {item}
-        </P.ProjectNumberItem>
+        <A.Now key={item} isSelected={selectedProject === item} onClick={() => handleClick(item)}>
+          <A.Num>{item}</A.Num>
+        </A.Now>
       ))}
-    </P.ProjectNumberContainer>
+    </A.SideBar>
   );
+}
+
+{
+  /* <A.SideBar>
+  {["4기", "3기", "2기", "1기"].map((item) => (
+    <A.Now key={item} onClick={() => handleNum(item)} isactive={selectedNumber === item} as="div">
+      <A.Num>{item}</A.Num>
+    </A.Now>
+  ))}
+</A.SideBar>; */
 }
